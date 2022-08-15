2e1d2162fcfe3fdfdcf1861b59e27dec2274b4f2
xavsln <19362582+xavsln@users.noreply.github.com>
2022-08-15 15:43:21 +0200

    Update links to images

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
-// import { kagemusha_poster } from './images/kagemusha_poster.jpeg';
-
 export class MainView extends React.Component {
   constructor(){
     super();
@@ -11,7 +9,7 @@ export class MainView extends React.Component {
       movies: [
         { _id: 1, Title: 'Tokyo Story', Description: 'The elderly Shukishi (Chishu Ryu) and his wife, Tomi (Chieko Higashiyama), take the long journey from their small seaside village to visit their adult children in Tokyo. Their elder son, Koichi (Sô Yamamura), a doctor, and their daughter, Shige (Haruko Sugimura), a hairdresser, don\'t have much time to spend with their aged parents, and so it falls to Noriko (Setsuko Hara), the widow of their younger son who was killed in the war, to keep her in-laws company....', ImagePath: 'https://upload.wikimedia.org/wikipedia/commons/1/1b/Tokyo-story-20201121.jpg' },
         { _id: 2, Title: 'Blade Runner', Description: 'Rick Deckard, an ex-policeman, becomes a special agent with a mission to exterminate a group of violent androids. As he starts getting deeper into his mission, he questions his own identity.', ImagePath: 'https://upload.wikimedia.org/wikipedia/en/9/9f/Blade_Runner_%281982_poster%29.png1'},
-        { _id: 3, Title: 'Kagemusha', Description: 'A petty thief is forced to choose between death and impersonating a warlord. His initial enjoyment gives way as war approaches.', ImagePath: 'images/kagemusha_poster.jpeg' }
+        { _id: 3, Title: 'Kagemusha', Description: 'A petty thief is forced to choose between death and impersonating a warlord. His initial enjoyment gives way as war approaches.', ImagePath: 'https://via.placeholder.com/150' }
       ]
     }
   }
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -8,7 +8,7 @@ export class MovieView extends React.Component {
     return (
       <div className="movie-view">
         <div className="movie-poster">
-          <img src={movie.ImagePath} />
+          <img src={movie.ImagePath} alt="Poster image of the movie" />
         </div>
         <div className="movie-title">
           <span className="label">Title: </span>
