fb3347adcf296008f6f12ea60dfe752b1e77a124
xavsln <19362582+xavsln@users.noreply.github.com>
2022-08-15 18:38:19 +0200

    Update to solve issue related to crossorigin

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -8,7 +8,7 @@ export class MovieView extends React.Component {
     return (
       <div className="movie-view">
         <div className="movie-poster">
-          <img src={movie.ImagePath} alt="Poster image of the movie" />
+          <img src={movie.ImagePath} alt="Poster image of the movie" crossorigin="anonymous" />
         </div>
         <div className="movie-title">
           <span className="label">Title: </span>
