caa58987ac58cdfa6b7a25edb9b82adc73e86ce7
xavsln <19362582+xavsln@users.noreply.github.com>
2022-08-16 13:39:31 +0200

    Implement login-view to allow users to log in

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import axios from 'axios';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import { LoginView } from '../login-view/login-view';
 
 export class MainView extends React.Component {
   constructor(){
     super();
     this.state = {
       movies: [],
-      selectedMovie: null
+      selectedMovie: null,
+      user: null
     }
   }
 
@@ -30,8 +32,16 @@ export class MainView extends React.Component {
     });
   }
 
+  onLoggedIn(user) {
+    this.setState({
+      user
+    });
+  }
+
   render() {
-    const { movies, selectedMovie } = this.state;
+    const { movies, selectedMovie, user } = this.state;
+
+    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
   
     if (movies.length === 0) return <div className="main-view" />;
   
